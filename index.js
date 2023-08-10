50b362812dba9067428a5d92b4baae2049747d6b
adithc <adith.chandraiah@gmail.com>
2023-08-09 21:33:17 -0500

    initial

---
+++ index.js
@@ -0,0 +1,7 @@
+// Get a reference to the button element
+const myButton = document.getElementById('myButton');
+
+// Add a click event listener to the button
+myButton.addEventListener('click', () => {
+    alert('Button clicked!');
+});
